347e1e50cb472a16446a4e03f7a4c25c635f0d5a
AlxisHenry <alexis.henry150357@gmail.com>
2022-06-28 15:52:56 +0200

    Change animations / Start click test

--- src/components/Button-Click/button-click.js
+++ src/components/Button-Click/button-click.js
@@ -1,12 +1,28 @@
 import './button-click.css'
+import React from "react";
 
-const ButtonClick = ({ content }) => {
-    return (
-        <div className="click-button-container" data-content={content}>
-            <button className={"btn"}>{ content }</button>
-        </div>
-    )
+class ButtonClick extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    CountClicks(el) {
+        if (el.target.classList.contains('start-click-test')) {
+            el.target.classList.remove('start-click-test')
+            el.target.innerText = ''
+        }
+    }
+
+    render()
+    {
+        return (
+            <div className="click-button-container" data-content={this.props.content}>
+                <button onClick={this.CountClicks.bind(this)} className={"btn click-button start-click-test"}>{this.props.content}</button>
+            </div>
+        )
+    }
+
 }
-
 
 export default ButtonClick
--- src/views/Click/Click.js
+++ src/views/Click/Click.js
@@ -1,12 +1,13 @@
-import ButtonClick from "../../components/Button-Click/button-click";
-import React from "react";
+import ButtonClick from "../../components/Button-Click/button-click"
+import React from "react"
+import './Click.css'
 
 export class ClickTest extends React.Component {
     render() {
         return (
-            <>
-                <ButtonClick content={"Click Here"}/>
-            </>
+            <section className={"click-page-content"}>
+                <ButtonClick content={"Click Here For Start"}/>
+            </section>
         )
     }
 }
