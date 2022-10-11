1ab1c3c474b2044b748557a6131afc1f455803e1
AlxisHenry <alexis.henry150357@gmail.com>
2022-10-11 15:23:44 +0200

    Timer & Click counter

--- src/components/ButtonClick.js
+++ src/components/ButtonClick.js
@@ -2,11 +2,17 @@
  * React
  */
 import React from "react";
+import { useState, useEffect } from 'react';
 
 /**
  * Css
  */
 import './styles/ButtonClick.css'
+
+/**
+ * Components
+ */
+import Scoreboard from "./Scoreboard"
 
 /**
  * Component : ButtonClick
@@ -15,22 +21,56 @@ export default class ButtonClick extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {clicks: 0}
+        this.state = {
+            clicks: 0,
+            timerStatus: false,
+            timer: 0
+        }
+    }
+    
+    start() {
+        this.startTimer()
+        this.click()
+    }
+    
+    startTimer() {
+        if (!this.state.timerStatus) {
+            this.setState(() => ({
+                timerStatus: true
+            }))
+
+            let upTimer = setInterval(this.checkTimer.bind(this), 10)
+        } 
     }
 
-    incrementClicks(e) {
+    checkTimer() {
+        if (this.state.timer === 10) {
+            clearInterval("upTimer")
+        }
+        this.setState((state, props) => ({
+            timer: parseFloat((state.timer + 0.01).toFixed(2))
+        }))
+    }
+
+    click() {
         this.setState((state, props) => ({clicks: state.clicks + 1}))
     }
 
     render()
     {
         return (
-            <div className="click-button-container" data-content={this.props.content}>
-                <button onClick={this.incrementClicks.bind(this)} className={"btn click-button start-click-test"}>
-                    {this.props.content}
-                </button>
-            </div>
+            <>
+                <div className="button__container" data-content={this.props.content}>
+                    <button 
+                    onClick={this.start.bind(this)} 
+                    className={"btn"}>
+                        {this.props.content}
+                    </button>
+                </div>
+                <Scoreboard count={this.state.clicks} timer={this.state.timer} />
+            </>
         )
     }
+
 
 }
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -15,20 +15,14 @@ export default class Scoreboard extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {clicks: 0}
-    }
-
-    incrementClicks(e) {
-        this.setState((state, props) => ({clicks: state.clicks + 1}))
     }
 
     render()
     {
         return (
-            <div className="click-button-container" data-content={this.props.content}>
-                <button onClick={this.incrementClicks.bind(this)} className={"btn click-button start-click-test"}>
-                    {this.props.content}
-                </button>
+            <div className="user__scoreboard">
+                { this.props.count } 
+                { this.props.timer } 
             </div>
         )
     }
